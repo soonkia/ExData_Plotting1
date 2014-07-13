5e67605eacb006c1b1ac631127e7cc2a52e84c96
Gerard Korsten <Soonkia77@gmail.com>
2014-07-13 19:10:40 +0200

    Plot 1 draws a nice Active Power Histogram

---
+++ plot1.R
@@ -0,0 +1,19 @@
+plot1 <- function(data) {
+  #Open png graphics device
+  png(file = "plot1.png", width=480, height=480, units="px")
+  #Create a histogram plot
+  hist(x$Global_active_power,xlab="Global Active Power (kilowatts)",col="red",main="Global Active Power")
+  #Switch the png graphics device off
+  dev.off()
+}
+
+#Main function to run and load the file
+runplot1 <-function (){
+  #Get the load code
+  source("plotting1.R")
+  #Read in the file
+  x<-readFile()
+  #plot and save to plot1.png
+  plot1(x)
+  
+}
